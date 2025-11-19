946f3e56959959b4f4025e2502e998fabbde1e93
Ryuichiro Suzuki <dragon.aka.scooter@gmail.com>
2025-11-19 18:14:02 +0900

    fair split

---
+++ app/utils/fairSplit.ts
@@ -0,0 +1,15 @@
+export const fairSplit = (total: number, count: number) => {
+  if (count <= 0 || total <= 0) {
+    return [];
+  }
+
+  const base = Math.floor(total / count);
+  const split = Array(count).fill(base);
+  const remainder = total % base;
+
+  for (let i = 0; i < remainder; i += 1) {
+    split[i] += 1;
+  }
+
+  return split;
+};
