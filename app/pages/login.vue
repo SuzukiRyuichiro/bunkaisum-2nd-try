3d210fbcfe98e63b539b6e507e371a9dcba126e5
Ryuichiro Suzuki <dragon.aka.scooter@gmail.com>
2025-11-25 12:39:08 +0900

    position login button

--- app/pages/login.vue
+++ app/pages/login.vue
@@ -1,16 +1,18 @@
 <template>
-  <AuthState v-slot="{ loggedIn, clear }">
-    <UButton v-if="loggedIn" @click="clear">Logout</UButton>
-    <UButton
-      v-else
-      icon="i-simple-icons-line"
-      size="md"
-      color="primary"
-      variant="solid"
-      @click="handleLogin"
-      >LINEでログイン</UButton
-    >
-  </AuthState>
+  <div class="h-screen grid place-items-center">
+    <AuthState v-slot="{ loggedIn, clear }">
+      <UButton v-if="loggedIn" @click="clear">Logout</UButton>
+      <UButton
+        v-else
+        icon="i-simple-icons-line"
+        size="md"
+        class="bg-[#06c755] hover:bg-[#04b54e]"
+        variant="solid"
+        @click="handleLogin"
+        >LINEでログイン</UButton
+      >
+    </AuthState>
+  </div>
 </template>
 
 <script setup lang="ts">
