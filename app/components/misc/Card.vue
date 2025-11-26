de6978469813c05982d65216ba38a425939ae890
Ryuichiro Suzuki <dragon.aka.scooter@gmail.com>
2025-11-26 17:09:40 +0900

    I think I can make a expense now

--- app/components/misc/Card.vue
+++ app/components/misc/Card.vue
@@ -1,8 +1,21 @@
 <template>
-  <div v-if="$slots.tag" class="text-sm text-slate-600 px-1">
-    <slot name="tag" />
-  </div>
-  <div class="bg-white rounded-2xl p-4 shadow-sm border border-slate-200">
-    <slot />
+  <div ref="root">
+    <div v-if="$slots.tag" class="text-sm text-slate-600 px-1 mb-3">
+      <slot name="tag" />
+    </div>
+    <div
+      v-bind:class="$attrs.class"
+      class="bg-white rounded-2xl p-4 shadow-sm border border-slate-200"
+    >
+      <slot />
+    </div>
   </div>
 </template>
+
+<script setup>
+import { ref, defineExpose } from "vue";
+
+const root = ref(null);
+
+defineExpose({ root });
+</script>
