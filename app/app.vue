df9b7450d96be253494a109f5f24d9eab4a404e5
Ryuichiro Suzuki <dragon.aka.scooter@gmail.com>
2025-12-16 14:48:40 +0400

    PWA working

--- app/app.vue
+++ app/app.vue
@@ -11,4 +11,27 @@
 import type { ToasterProps } from "@nuxt/ui";
 
 const toaster: ToasterProps = { position: "bottom-right" };
+
+useHead({
+  link: [
+    {
+      rel: "manifest",
+      href: "/manifest.webmanifest",
+    },
+  ],
+  meta: [
+    {
+      name: "theme-color",
+      content: "#ffffff",
+    },
+    {
+      name: "apple-mobile-web-app-capable",
+      content: "yes",
+    },
+    {
+      name: "apple-mobile-web-app-status-bar-style",
+      content: "default",
+    },
+  ],
+});
 </script>
