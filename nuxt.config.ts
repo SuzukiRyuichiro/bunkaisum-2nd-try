3a6696deeaa3ce8fc6f563eb22dee7d4e9e0fa2e
Ryuichiro Suzuki <dragon.aka.scooter@gmail.com>
2025-12-16 15:39:59 +0400

    some updates about path

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -36,7 +36,7 @@ export default defineNuxtConfig({
     manifest: {
       name: "Bunkaisum - シェアハウスの割り勘",
       short_name: "Bunkaisum",
-      description: "僕が作ったアプリ",
+      description: "シェアハウスの支出を簡単に割り勘できるアプリ",
       theme_color: "#ffffff",
       background_color: "#ffffff",
       display: "standalone",
@@ -44,13 +44,13 @@ export default defineNuxtConfig({
       id: "/",
       icons: [
         {
-          src: "android/android-launchericon-192-192.png",
+          src: "/android/android-launchericon-192-192.png",
           sizes: "192x192",
           type: "image/png",
           purpose: "any maskable",
         },
         {
-          src: "android/android-launchericon-512-512.png",
+          src: "/android/android-launchericon-512-512.png",
           sizes: "512x512",
           type: "image/png",
           purpose: "any maskable",
@@ -58,14 +58,14 @@ export default defineNuxtConfig({
       ],
       screenshots: [
         {
-          src: "screenshots/groups.png",
+          src: "/screenshots/groups.png",
           sizes: "846x808",
           type: "image/png",
           form_factor: "wide",
           label: "Groups page",
         },
         {
-          src: "screenshots/expenses.png",
+          src: "/screenshots/expenses.png",
           sizes: "846x1598",
           type: "image/png",
           form_factor: "narrow",
