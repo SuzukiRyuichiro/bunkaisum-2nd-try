6ac1e4eb52a2b03a4cd835d5ae8084263f3a87ff
Ryuichiro Suzuki <dragon.aka.scooter@gmail.com>
2026-03-19 13:22:26 +0900

    minor typing

--- server/api/users/me.get.ts
+++ server/api/users/me.get.ts
@@ -1,9 +1,10 @@
 import { eq } from "drizzle-orm";
 import { usersTable } from "~~/server/db/schema";
 import useDrizzle from "~~/server/utils/drizzle";
+import { TokenPayload } from "~~/server/utils/jwt";
 
 export default defineEventHandler(async (event) => {
-  const userId = (event.context.user as { userId: number })?.userId;
+  const userId = (event.context.user as TokenPayload)?.userId;
   if (!userId) {
     throw createError({ statusCode: 401, statusMessage: "Unauthorized" });
   }
